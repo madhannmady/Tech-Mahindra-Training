5b2e2776af3c32c4de98a76e70192318d119b707
Madhan kumar.K <madhankumark763@gmail.com>
2025-02-14 12:53:40 +0530

    Upload Workout file10

---
+++ JavaScript/workouts/file10.js
@@ -0,0 +1,13 @@
+let mark = 88;
+
+if (mark === 100) {
+  console.log("Phenomenal");
+} else if (mark > 85) {
+  console.log("Excellent ");
+} else if (mark <= 85 && mark >= 71) {
+  console.log("Good Work");
+} else if (mark <= 70 && mark >= 51) {
+  console.log("Practice Well");
+} else {
+  console.log("Hard Work is needed");
+}
